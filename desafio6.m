4a1fcd1becf4dbbf3848292f7d18e9426114d26b
Nathalie <106367468+nathusvel09@users.noreply.github.com>
2024-09-10 17:26:31 -0400

    Delete desafio6.m

--- desafio6.m
+++
@@ -1,8 +0,0 @@
-% Definir la matriz (A)
-a = [  0.52, 0.20, 0.25; 0.30, 0.50, 0.20; 0.18, 0.30, 0.55];
-% Definir el vector de las cantidades requeridas (b)
-b = [4800; 5810; 5690];
-% Resolver el sistema de ecuaciones
-x = inv(a)*b;
-x1 = a \ b;
-
